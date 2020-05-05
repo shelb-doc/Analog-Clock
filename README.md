34f9bc8b4672a50b45cef39a390c5e8d8c139726
Shelby Cignetti <shelbycignetti@gmail.com>
2020-05-04 21:27:34 -0400

    Added Color changes depending on time of day.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Analog-Clock
 An analog clock made using Html, Css and Javascript.
+The Clock will change the CSS depending on the time of Day.
--- clock.js
+++ clock.js
@@ -2,13 +2,31 @@ const deg = 6;
 const hr = document.querySelector('#hr');
 const mn = document.querySelector('#mn');
 const sc = document.querySelector('#sc');
+const clock = document.querySelector('.clock');
+const body = document.querySelector('body');
 
 setInterval(() => {
     let day = new Date();
-    let hh = day.getHours() * 30;
+    let hour = day.getHours()
+    let hh = hour * 30;
     let mm = day.getMinutes() * deg;
     let ss = day.getSeconds() * deg;
     hr.style.transform = `rotateZ(${hh + mm / 12}deg)`;
     mn.style.transform = `rotateZ(${mm}deg)`;
     sc.style.transform = `rotateZ(${ss}deg)`;
+    if((hour >= 1 ) && (hour <= 11)) {
+        document.title = 'Good morning'
+        clock.style.borderColor = 'lightskyblue';
+        body.style.background = 'linear-gradient(to right, #1c3c72, #2a5298)'
+    }
+    else if ((hour >= 12 ) && (hour <= 16)) {
+        document.title = 'Good afternoon'
+        clock.style.borderColor = 'lightsalmon';
+        body.style.background = 'linear-gradient(to right, #721c1c, #982a2a)'
+    }
+    else {
+        document.title = 'Good evening'
+        clock.style.borderColor = 'grey';
+        body.style.background = 'linear-gradient(to right, #23262a, #474b52)'
+    }
 });
